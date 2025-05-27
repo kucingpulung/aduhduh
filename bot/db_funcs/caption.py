7b575b801debddde6445362a921dac4a8e32702b
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2025-05-27 18:38:14 +0700

    Update caption.py

--- bot/db_funcs/caption.py
+++ bot/db_funcs/caption.py
@@ -1,18 +1,14 @@
 from bot.base import database
 from bot.utils import BOT_ID
 
-
 CAPTION_KEY = "CUSTOM_CAPTION"
-
 
 async def set_custom_caption(template: str) -> None:
     await database.add_value(int(BOT_ID), CAPTION_KEY, template)
 
-
 async def get_custom_caption() -> str:
     doc = await database.get_doc(int(BOT_ID))
     return doc.get(CAPTION_KEY, ["{original_caption}"])[0]
-
 
 async def reset_custom_caption() -> None:
     await database.clear_value(int(BOT_ID), CAPTION_KEY)
